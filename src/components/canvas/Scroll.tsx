2f702c55c630650b290b4200fad3dfe06785940e
Kenny Stanley <kpstanleyjr@gmail.com>
2022-01-06 16:37:38 -0600

    removed onDrag listener to use mapControls

--- src/components/canvas/Scroll.tsx
+++ src/components/canvas/Scroll.tsx
@@ -1,11 +1,10 @@
 import { useSpring, config } from '@react-spring/core'
 import { useGesture } from 'react-use-gesture'
 import { useCallback, useEffect } from 'react'
-// import { clamp } from 'three/src/math/MathUtils'
 import clamp from 'lodash/clamp'
 
 export function Scroll(bounds: [number, number], props: { domTarget: any }) {
-	const [{ y }, set] = useSpring(() => ({ y: 0, config: config.slow }))
+	const [{ y }, set] = useSpring(() => ({ y: 200, config: config.default }))
 	const fn = useCallback(
 		({ xy: [, cy], previous: [, py], memo = y.get() }) => {
 			const newY = clamp(memo + cy - py, ...bounds)
@@ -14,7 +13,7 @@ export function Scroll(bounds: [number, number], props: { domTarget: any }) {
 		},
 		[bounds, y, set]
 	)
-	const bind = useGesture({ onWheel: fn, onDrag: fn }, props)
+	const bind = useGesture({ onWheel: fn }, props)
 	useEffect(() => props && props.domTarget && bind(), [props, bind])
 	return [y, bind]
 }
